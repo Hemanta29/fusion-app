fd8e270fcfce0601e4cbb5a8ee171ab82e0f05aa
Hemanta <hemantadutta29@gmail.com>
2022-12-29 22:54:01 +0530

    Animations Part 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,17 @@
 import { Leader } from './../shared/leader';
 import { LeaderService } from './../services/leader.service';
 import { Component, Inject } from '@angular/core';
+import { expand, flyInOut } from '../animations/app.animation';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [flyInOut(), expand()]
 })
 export class AboutComponent {
   leaders: Leader[] = [];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,25 +8,13 @@ import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { flyInOut, visibility, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
-  animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
-  ]
+  animations: [visibility(), flyInOut(), expand()]
 })
 export class DishdetailComponent implements OnInit {
   @Input() dish!: Dish;
